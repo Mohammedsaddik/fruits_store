77beac654c6ea72b22b9fa74092a56d035edb76a
Mohammedsaddik <mohamedsaddik586@gmail.com>
2025-03-05 01:13:31 +0200

    create SearchTextField

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_store/Features/home/presentation/views/widgets/custom_home_app_bar.dart';
+import 'package:fruits_store/Features/home/presentation/views/widgets/search_text_field.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -22,6 +23,7 @@ class HomeViewBody extends StatelessWidget {
                 SizedBox(
                   height: 16.0,
                 ),
+                SearchTextField(),
               ],
             ),
           ),
